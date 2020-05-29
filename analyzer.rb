e35c696a51c27aca545f3a133a5527b453047f68
Ihsaanul Hoque <ihsaanul.hoque@hotmail.com>
2020-05-30 00:16:11 +0100

    tidied presentation

--- analyzer.rb
+++ analyzer.rb
@@ -1,25 +1,24 @@
 # counting number of lines
 lines = File.readlines('text.txt')
 line_count = lines.size
-puts "#{line_count} lines"
 
 text = lines.join
 
 # counting characters
 total_characters = text.length
-puts "#{total_characters} characters"
-
 # counting characters without whitespace
 total_characters_nospaces = text.gsub(/\s+/, '').length
-puts "#{total_characters_nospaces} characters (excluding whitespace)"
-
 # counting words
 word_count = text.split.length
-puts "#{word_count} words"
-
 # counting sentences and paragraphs
 paragraph_count = text.split(/\n\n/).length
-puts "#{paragraph_count} paragraphs"
-
 sentence_count = text.split(/\.|\?|!/).length
+
+puts "#{line_count} lines"
+puts "#{total_characters} characters"
+puts "#{total_characters_nospaces} characters (excluding whitespace)"
+puts "#{word_count} words"
+puts "#{paragraph_count} paragraphs"
 puts "#{sentence_count} sentences"
+puts "#{sentence_count / paragraph_count} sentences per paragraph (average)"
+puts "#{word_count / sentence_count} words per sentence (average)"
