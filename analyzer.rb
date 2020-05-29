d9008766c175616d4b2c387fd68bac574062dcd6
Ihsaanul Hoque <ihsaanul.hoque@hotmail.com>
2020-05-29 23:00:55 +0100

    counting characters

--- analyzer.rb
+++ analyzer.rb
@@ -4,3 +4,11 @@ line_count = lines.size
 puts "#{line_count} lines"
 
 text = lines.join
+
+# counting characters
+total_characters = text.length
+puts "#{total_characters} characters"
+
+# counting characters without whitespace
+total_characters_nospaces = text.gsub(/\s+/, '').length
+puts "#{total_characters_nospaces} characters (excluding whitespace)"
