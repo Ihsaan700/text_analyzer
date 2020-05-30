61510e58b10fea14c06c6d7f48209191c164bf84
Ihsaanul Hoque <ihsaanul.hoque@hotmail.com>
2020-05-30 01:19:00 +0100

    percentage of useful words

--- analyzer.rb
+++ analyzer.rb
@@ -13,6 +13,12 @@ word_count = text.split.length
 # counting sentences and paragraphs
 paragraph_count = text.split(/\n\n/).length
 sentence_count = text.split(/\.|\?|!/).length
+# percentage of useful words
+stopwords = %w[the a by on for of are with just but and to the my I has some in]
+words = text.scan(/\w+/)
+good_words = words.select { |word| !stopwords.include?(word) }
+good_percentage = ((good_words.length.to_f / words.length.to_f) * 100).to_i
+
 
 puts "#{line_count} lines"
 puts "#{total_characters} characters"
@@ -22,3 +28,4 @@ puts "#{paragraph_count} paragraphs"
 puts "#{sentence_count} sentences"
 puts "#{sentence_count / paragraph_count} sentences per paragraph (average)"
 puts "#{word_count / sentence_count} words per sentence (average)"
+puts "#{good_percentage}% of words are non-fluff words"
